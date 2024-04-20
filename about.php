d1a5ed0921cd03bc2105a28d2e141b60fa8e65d1
marlon1412 <marlonarcos.orpiada@bicol-u.edu.ph>
2024-04-21 00:09:13 +0800

    update about page ui

--- about.php
+++ about.php
@@ -57,20 +57,15 @@ if(isset($_SESSION['user_id'])){
    <div class="row">
 
       <div class="image">
-         <img src="/images/fish/fish2.png" alt="">
+         <img src="/images/book/Reading book.png" alt="">
       </div>
 
       <div class="content">
          <h3>Why choose us?</h3>
          <p>
-            Welcome to Tep's FishyFlex, where our love for aquatic life meets your passion for fishkeeping! 
-            At Tep's, we're not just a pet shop; we're a community of dedicated fish enthusiasts committed to providing you with a top-notch experience. With a team of knowledgeable experts, 
-            we ensure the health and happiness of every fish we offer. 
-            Dive into a sea of possibilities with our wide selection of vibrant fish species and high-quality aquarium supplies. 
-            What sets us apart is our customer-centric approach—we're here to assist whether you're a seasoned aquarium enthusiast or a first-time fish owner. Join our community, share experiences, and let 
-            Tep's FishyFlex be your trusted partner in creating the perfect underwater haven. Thank you for choosing Tep's FishyFlex, where passion and expertise come together for an immersive aquatic journey!
+         Welcome to BookLat, your premier online destination for all things books! At BookLat, we're passionate about literature and committed to bringing you an unparalleled reading experience. With an extensive collection spanning diverse genres, from beloved classics to the latest bestsellers, there's something for every reader to explore. What sets us apart? Our innovative rental feature offers affordability and flexibility, allowing you to enjoy your favorite titles without the commitment of a full purchase. But BookLat is more than just a bookstore; it's a vibrant community of book enthusiasts. Join us to share recommendations, engage in discussions, and connect with fellow readers who share your passion. From seamless checkout to prompt customer support, our dedicated team is here to ensure your satisfaction every step of the way. Discover the joy of reading anew with BookLat, where every page turns into an adventure!
          </p>
-         <a href="menu.php" class="btn">Our Fishes</a>
+         <a href="menu.php" class="btn">Check our Books</a>
       </div>
 
    </div>
@@ -84,7 +79,7 @@ if(isset($_SESSION['user_id'])){
 
 <section class="steps">
 
-   <h1 class="title">Simple steps</h1>
+   <h1 class="title">Get your Books NOW with these <br>Simple Steps</h1>
 
    <div class="box-container">
 
@@ -92,21 +87,23 @@ if(isset($_SESSION['user_id'])){
          <img src="images/fish/checkout.jpg" alt="">
          <h3>Checkout</h3>
          <p>
-            Complete your underwater vision effortlessly at Tep's FishyFlex checkout—where vibrant fish and top-notch aquarium supplies meet. Dive into satisfaction as you secure your aquatic essentials and join a community of passionate fish enthusiasts.</p>
+         Embark on a seamless literary journey at BookLat's checkout, where the magic of books meets seamless online shopping. Immerse yourself in satisfaction as you secure your favorite reads and join a vibrant community of fellow book enthusiasts.    
+      </p>
       </div>
 
       <div class="box">
          <img src="images/fish/deliver.jpg" alt="">
          <h3>Fast delivery</h3>
          <p>
-         Experience the anticipation of aquatic wonders heading your way with Tep's FishyFlex swift and secure delivery service, ensuring your vibrant fish and premium aquarium supplies reach your doorstep in pristine condition.         </p>
+         Experience the thrill of anticipation as your literary treasures swiftly make their way to your doorstep with BookLat's reliable and secure delivery service. Trust us to ensure your books arrive in pristine condition, ready to enrich your reading experience.
+         </p>
       </div>
 
       <div class="box">
          <img src="images/fish/receive.jpg" alt="">
          <h3>Receive</h3>
          <p>
-         Unbox joy and introduce your underwater companions to their new home as you receive your Tep's FishyFlex delivery, thoughtfully packaged to guarantee the well-being of your aquatic friends and kickstart a journey of fishkeeping delight
+         Unwrap excitement and welcome your new literary companions into your home as you receive your BookLat delivery, meticulously packaged to ensure the safety of your beloved books and ignite a journey of reading pleasure.
 
          </p>
       </div>
@@ -130,7 +127,7 @@ if(isset($_SESSION['user_id'])){
          <div class="swiper-slide slide">
             <img src="images/pic-1.png" alt="">
             <p>
-            "Tep's FishyFlex has transformed my hobby with their diverse selection of vibrant fish and quality supplies. The impeccable delivery service ensures my aquatic companions arrive in perfect condition, making Tep's my go-to for all things fish-related."
+            "BookLat exceeded my expectations! The checkout process was a breeze, and my order arrived promptly. I'm impressed by the quality of service and will definitely be back for more reads."
          </p>
             <div class="stars">
                <i class="fas fa-star"></i>
@@ -139,12 +136,14 @@ if(isset($_SESSION['user_id'])){
                <i class="fas fa-star"></i>
                <i class="fas fa-star-half-alt"></i>
             </div>
-            <h3>john deo</h3>
+            <h3>Customer 1</h3>
          </div>
 
          <div class="swiper-slide slide">
             <img src="images/pic-2.png" alt="">
-            <p>"As a first-time fish owner, I was impressed by Tep's FishyFlex's knowledgeable and friendly team. The step-by-step guidance during the checkout process and the prompt delivery made my experience stress-free. Tep's has earned my trust for its commitment to customer satisfaction."</p>
+            <p>
+            "What a delightful experience with BookLat! My books were carefully packaged, and the delivery was quick. Highly recommended for book lovers!"
+            </p>
             <div class="stars">
                <i class="fas fa-star"></i>
                <i class="fas fa-star"></i>
@@ -152,12 +151,14 @@ if(isset($_SESSION['user_id'])){
                <i class="fas fa-star"></i>
                <i class="fas fa-star-half-alt"></i>
             </div>
-            <h3>john deo</h3>
+            <h3>Customer 2</h3>
          </div>
 
          <div class="swiper-slide slide">
             <img src="images/pic-3.png" alt="">
-            <p>"The health and happiness of the fish from Tep's FishyFlex truly stand out. My aquarium has flourished since introducing their vibrant fish, and the seamless delivery process ensures a smooth transition for the aquatic additions to my home."</p>
+            <p>
+            "I'm thrilled with my purchase from BookLat! The website is user-friendly, and the community aspect adds a nice touch. Looking forward to my next order!"
+            </p>
             <div class="stars">
                <i class="fas fa-star"></i>
                <i class="fas fa-star"></i>
@@ -165,25 +166,28 @@ if(isset($_SESSION['user_id'])){
                <i class="fas fa-star"></i>
                <i class="fas fa-star-half-alt"></i>
             </div>
-            <h3>john deo</h3>
+            <h3>Customer 3</h3>
          </div>
 
          <div class="swiper-slide slide">
             <img src="images/pic-4.png" alt="">
-            <p>"Tep's FishyFlex has created a community feel around fishkeeping. Their prompt and secure delivery not only brings quality products but also connects enthusiasts. I appreciate the personalized touch and sense of camaraderie Tep's brings to the fishkeeping community."</p>
+            <p>"Impressed by the efficiency of BookLat's delivery service! My books arrived in perfect condition, and the selection is fantastic. Five stars!"</p>
             <div class="stars">
                <i class="fas fa-star"></i>
                <i class="fas fa-star"></i>
                <i class="fas fa-star"></i>
                <i class="fas fa-star"></i>
-               <i class="fas fa-star-half-alt"></i>
+               <i class="fas fa-star"></i>
+               
             </div>
-            <h3>john deo</h3>
+            <h3>Customer 4</h3>
          </div>
 
          <div class="swiper-slide slide">
             <img src="images/pic-5.png" alt="">
-            <p>"I've tried various fish shops, but Tep's FishyFlex stands out for its dedication to customer care. The easy checkout process, reliable delivery, and the overall quality of their products make Tep's my preferred choice for all my aquarium needs."</p>
+            <p>
+            "Kudos to BookLat for a wonderful shopping experience! The customer service was top-notch, and the delivery was swift. I'll definitely be a returning customer."
+            </p>
             <div class="stars">
                <i class="fas fa-star"></i>
                <i class="fas fa-star"></i>
@@ -191,12 +195,15 @@ if(isset($_SESSION['user_id'])){
                <i class="fas fa-star"></i>
                <i class="fas fa-star-half-alt"></i>
             </div>
-            <h3>john deo</h3>
+            <h3>Customer 5</h3>
          </div>
 
          <div class="swiper-slide slide">
             <img src="images/pic-6.png" alt="">
-            <p>"Tep's FishyFlex delivers not just fish but an experience. The anticipation during the delivery step is met with excitement upon receiving healthy and vibrant aquatic companions. Tep's attention to detail and commitment to customer satisfaction make them a standout in the world of fishkeeping."</p>
+            <p>
+               "BookLat is a gem for book enthusiasts! The checkout process was seamless, and the variety of genres is impressive. Can't wait to dive into my new reads!"
+
+            </p>
             <div class="stars">
                <i class="fas fa-star"></i>
                <i class="fas fa-star"></i>
@@ -204,7 +211,7 @@ if(isset($_SESSION['user_id'])){
                <i class="fas fa-star"></i>
                <i class="fas fa-star-half-alt"></i>
             </div>
-            <h3>john deo</h3>
+            <h3>Customer 6</h3>
          </div>
 
       </div>
